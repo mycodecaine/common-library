e945a103fd7e18794a83da3e4878a3977c875012
heemi <heemi@softwareq.com>
2025-05-01 22:29:38 +0800

    Minor cleanup.

--- src/Implementation/Codecaine.SportService.Application/UseCases/SportTypes/Commands/CreateSportType/CreateSportTypeCommand.cs
+++ src/Implementation/Codecaine.SportService.Application/UseCases/SportTypes/Commands/CreateSportType/CreateSportTypeCommand.cs
@@ -1,12 +1,13 @@
 ﻿using Codecaine.Common.CQRS.Commands;
 using Codecaine.Common.Primitives.Result;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Codecaine.SportService.Application.UseCases.SportTypes.Commands.CreateSportType
-{   
+{
+    /// <summary>
+    /// Command to create a new sport type.
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <param name="Description"></param>
+    /// <param name="ImageUrl"></param>
     public record CreateSportTypeCommand(string Name, string Description, string ImageUrl) :ICommand<Result<CreateSportTypeCommandResponse>>;
 }
--- src/Implementation/Codecaine.SportService.Application/UseCases/SportTypes/Commands/CreateSportType/CreateSportTypeCommandResponse.cs
+++ src/Implementation/Codecaine.SportService.Application/UseCases/SportTypes/Commands/CreateSportType/CreateSportTypeCommandResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Codecaine.SportService.Application.UseCases.SportTypes.Commands.CreateSportType
+﻿namespace Codecaine.SportService.Application.UseCases.SportTypes.Commands.CreateSportType
 {
     public record CreateSportTypeCommandResponse
     (
