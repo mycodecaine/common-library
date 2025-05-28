7a12d65d3d5b3e2fb6b56c301978ad1130afcd3f
heemi <heemi@softwareq.com>
2025-05-28 23:54:04 +0800

    Added GetPlayerId Handler

---
+++ src/Implementation/Codecaine.SportService.Application/UseCases/Players/Queries/GetPlayerById/GetPlayerByIdQueryHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Codecaine.Common.CQRS.Base;
+using Codecaine.Common.Primitives.Maybe;
+using Codecaine.SportService.Application.UseCases.SportTypes.Queries.GetSportTypeById;
+using Codecaine.SportService.Application.ViewModels;
+using Codecaine.SportService.Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codecaine.SportService.Application.UseCases.Players.Queries.GetPlayerById
+{
+   
+
+    internal class GetPlayerByIdQueryHandler : QueryHandler<GetPlayerByIdQuery, Maybe<PlayerViewModel>>
+    {
+        private readonly ILogger<GetPlayerByIdQueryHandler> _logger;
+        private readonly IPlayerRepository _playerRepository;
+        private readonly IMapper _mapper;
+
+        public GetPlayerByIdQueryHandler(ILogger<GetPlayerByIdQueryHandler> logger, IPlayerRepository playerRepository, IMapper mapper) : base(logger)
+        {
+            _logger = logger;
+            _playerRepository = playerRepository;
+            _mapper = mapper;
+        }
+
+        public override async Task<Maybe<PlayerViewModel>> Handle(GetPlayerByIdQuery request, CancellationToken cancellationToken) =>
+         await HandleSafelyAsync(async () =>
+         {
+             var playerResult = await _playerRepository.GetByIdAsync(request.Id);
+             if (playerResult.HasNoValue)
+             {
+                 _logger.LogWarning("Player with id: {Id} not found", request.Id);
+                 return Maybe<PlayerViewModel>.None;
+             }
+
+             var player = playerResult.Value;
+             var playerViewModel = _mapper.Map<PlayerViewModel>(player);
+             return playerViewModel;
+         });
+    }
+}
--- src/Implementation/Codecaine.SportService.Presentation.WebApi/Controllers/PlayersController.cs
+++ src/Implementation/Codecaine.SportService.Presentation.WebApi/Controllers/PlayersController.cs
@@ -1,8 +1,12 @@
 ﻿using Asp.Versioning;
 using Codecaine.Common.Errors;
+using Codecaine.Common.Primitives.Maybe;
 using Codecaine.Common.Primitives.Result;
 using Codecaine.SportService.Application.UseCases.Players.Commands.CreatePlayer;
+using Codecaine.SportService.Application.UseCases.Players.Queries.GetPlayerById;
 using Codecaine.SportService.Application.UseCases.SportTypes.Commands.CreateSportType;
+using Codecaine.SportService.Application.UseCases.SportTypes.Queries.GetSportTypeById;
+using Codecaine.SportService.Application.ViewModels;
 using Codecaine.SportService.Presentation.WebApi.DTOs.Players;
 using Codecaine.SportService.Presentation.WebApi.DTOs.SportTypes;
 using MediatR;
@@ -29,5 +33,15 @@ namespace Codecaine.SportService.Presentation.WebApi.Controllers
              .Map(request => new CreatePlayerCommand(request.Name, request.Description, request.ImageUrl))
              .Bind(command => Mediator.Send(command))
              .Match(Ok, BadRequest);
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(PlayerViewModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(Guid id) =>
+         await Maybe<GetPlayerByIdQuery>
+             .From(new GetPlayerByIdQuery(id))
+             .Bind(query => Mediator.Send(query))
+             .Match(Ok, NotFound);
     }
 }
